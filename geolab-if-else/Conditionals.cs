0ea55a0c483a7062bb56649bf4bbf3c972a61a3f
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-23 13:28:04 +0400

    create conditional namespace for conditional statement methods

---
+++ geolab-if-else/Conditionals.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geolab_if_else
+{
+    internal class Conditionals
+    {
+
+        public string typeOfNumber(long number)
+        {
+            if (number > 0)
+            {
+                return "number is positive";
+            }
+            else if (number < 0)
+            {
+                return "number is negative";
+            }
+            else
+            {
+                return "number is zero";
+            }
+        }
+
+      
+    }
+}
