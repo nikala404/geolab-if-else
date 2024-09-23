a59ffc9ca4043fc64eb947b1b81b86c4f75531a7
nikala404 <nikoloz.lursmanashvili.1@btu.edu.ge>
2024-09-23 14:22:49 +0400

    added: isNumberEvenOrOdd,isYearLeap, studentRate and passwordTester

--- geolab-if-else/Conditionals.cs
+++ geolab-if-else/Conditionals.cs
@@ -25,6 +25,73 @@ namespace geolab_if_else
             }
         }
 
+        public string isNumberEvenOrOdd(long number)
+        {
+            if (number % 2 == 0)
+            {
+                return "number is even";
+            }
+            else
+            {
+                return "number is odd";
+            }
+        }
+
+        public string studentRate(float number) {
+        
+            if(number >= 90 && number <= 100)
+            {
+                return "A";
+            } else if(number >= 80 && number <= 89)
+            {
+                return "B";
+            } else if(number >= 70 && number <= 79)
+            {
+                return "C";
+            } else if(number >= 60 && number <= 69)
+            {
+                return "D";
+            } else if(number >= 0 && number <= 59)
+            {
+                return "F";
+            }
+             else
+            {
+                return "Invalid number";
+            }   
+        }
+
+        public string IsYearLeap(int year)
+        {
+            string result = "";
+            if(year % 4 == 0 & year % 100 != 0)
+            {
+                if(year % 400 == 0)
+                {
+
+                    result = "This year is Leap";
+                }
+            }
+            else
+            {
+                result =  "This year is not leap";
+            }
+            return result;
+
+        }
+
+        public void passwordTester(string password, string usersInput)
+        {
+            if(usersInput == password)
+            {
+                Console.WriteLine("Access Granted");
+            }
+            else
+            {
+                Console.WriteLine("Access Forbidden");
+            }
+        }
+
       
     }
 }
--- geolab-if-else/Program.cs
+++ geolab-if-else/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using geolab_if_else;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Conditionals conditionals = new Conditionals();
+
+        Console.WriteLine(conditionals.typeOfNumber(5));
+        Console.WriteLine(conditionals.isNumberEvenOrOdd(22));
+        Console.WriteLine(conditionals.IsYearLeap(2024));
+        Console.WriteLine(conditionals.studentRate(12));
+
+        Console.WriteLine("Enter your password:");
+        string input = Console.ReadLine();
+        conditionals.passwordTester("nika123", input);   
+
+
+
+
+    }
+
+  
+}
